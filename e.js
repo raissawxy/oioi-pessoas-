34dc51ad3b9cd2086b355cafd0622fcfa9fcd109
Raissa Aparecida Gessner <raissagessner13@gmail.com>
2024-06-19 21:46:11 -0300

    primeiro exercicio em javascript

---
+++ e.js
@@ -0,0 +1,33 @@
+let primeironome='joão'
+console.log (primeironome)
+
+let a=5, b=10
+console.log(5+10)
+
+primeironome= 'sthefany', segundonome='diehl', junta=primeironome+segundonome
+console.log(junta)
+
+let pessoa={nome:'sthefanydiehl',idade:15,endereço: 408,telefone:47999553171}
+console.log(pessoa)
+
+let frutas=['maça', 'banana', 'laranja']
+frutas.push('uva')
+console.log(frutas)
+
+let numeros=[1,2,3,4,5]
+console.log(numeros[numeros.lenght-1])
+
+let cores=['vermelho', 'verde', 'azul']
+for(let i=0;i<cores.lenght;i++) 
+    console.log(cores[i])
+
+let idade=26
+if (idade<12)
+    console.log('criança')
+else if (idade<18)
+    console.log (adolescente)
+else if (idade<60)
+    console.log (adulto)
+else
+console.log (idoso)
+
